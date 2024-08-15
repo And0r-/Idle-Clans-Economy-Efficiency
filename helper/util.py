6895905a706a98a27d6ab884cb123dc26c1329f1
Lyon-K <keel@oregonstate.edu>
2024-08-14 18:12:43 -0700

    Added venv ignoring

--- helper/util.py
+++ helper/util.py
@@ -1,5 +1,11 @@
 import json
 
+
 def fileToJson(filename):
-    with open(filename, 'r') as json_file:
+    with open(filename, "r") as json_file:
         return json.load(json_file)
+
+
+def getAllItems(filepathToConfigData):
+    # Path: \AppData\LocalLow\isam_games\Idle Clans\Production\configData.json
+    pass
